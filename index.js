98adbf84a9bddc410863498579ae400dbfce95ee
saifsamir-k <59330324+saifsamir-k@users.noreply.github.com>
2021-07-09 12:50:27 +0200

    Adding the server's main files

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+// Import the necessary packages to be used all over
+// index.js
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// Initialize our express app that we will build our
+// apis upon
 const app = express();
 
-// PORT
+// Initialize the port that we ought to connect
+// to and it is either the one in .env or port 4000
 const PORT = process.env.PORT || 4000;
 
+// Connect to the url of the apis we are to connect to
 app.get("/", (req, res) => {
   res.json({ message: "API Working" });
 });
 
+// Listen to the port we connect to so that the server
+// can set to work
 app.listen(PORT, (req, res) => {
   console.log(`Server Started at PORT ${PORT}`);
 });
