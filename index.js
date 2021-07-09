064a2e0da768bd9e1243dd82e0ad99b4875b8e1f
saifsamir-k <59330324+saifsamir-k@users.noreply.github.com>
2021-07-09 13:50:50 +0200

    Finishing user signup/login/get-me

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 // index.js
 const express = require("express");
 const bodyParser = require("body-parser");
+const user = require("./routes/user");
+const InitiateMongoServer = require("./db");
+
+// Initiate Mongo Server
+InitiateMongoServer();
 
 // Initialize our express app that we will build our
 // apis upon
@@ -11,10 +16,20 @@ const app = express();
 // to and it is either the one in .env or port 4000
 const PORT = process.env.PORT || 4000;
 
+// Middleware
+app.use(bodyParser.json());
+
 // Connect to the url of the apis we are to connect to
 app.get("/", (req, res) => {
   res.json({ message: "API Working" });
 });
+
+/**
+ * Router Middleware
+ * Router - /user/*
+ * Method - *
+ */
+app.use("/user", user);
 
 // Listen to the port we connect to so that the server
 // can set to work
